682c7ce07a3dc1d0e6e051bb21d1f66b3aff2a30
wolframwelschinger <wolfram.welschinger@web.de>
2020-04-04 19:11:51 +0200

    Update Product.java
    Introduced Lombok, move JPA-Mapping next to class attributes

--- src/main/java/com/example/crudwithvaadin/entity/Product.java
+++ src/main/java/com/example/crudwithvaadin/entity/Product.java
@@ -1,11 +1,11 @@
-package com.example.crudwithvaadin.entity;
-
-import javax.persistence.*;
-
-
 /**
  * Entity class for Product
  */
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "product")
 public class Product {
@@ -13,62 +13,16 @@ public class Product {
     @Id
     @GeneratedValue
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "price")
     private double price;
-    private ProductCategory category;
-
-
-    /**
-     * NoArgsConstructor
-     */
-    public Product() {
-
-    }
-
-    /**
-     * AllArgsConstructor
-     * @param name product name
-     * @param price product price
-     * @param category product category
-     */
-    public Product(String name, double price, ProductCategory category)  {
-        this.name = name;
-        this.price = price;
-        this.category = category;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "category_id", referencedColumnName = "id")
-    // @JsonIgnor
-    public ProductCategory getCategory() {
-        return category;
-    }
+    @JoinColumn(name = "productcategory_id", referencedColumnName = "id")
+    // @JsonIgnore
+    private ProductCategory productCategory;
 
-    public void setCategory(ProductCategory category) {
-        this.category = category;
-    }
 }
